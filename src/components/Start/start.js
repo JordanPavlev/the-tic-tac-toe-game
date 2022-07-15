638b83183f704a7dc66ed5b2a51a8c26502b3051
JordanPavlev <danipavlev@gmail.com>
2022-07-15 17:46:59 +0300

    start, TODO board

--- src/components/Start/start.js
+++ src/components/Start/start.js
@@ -10,8 +10,26 @@ export const Start = () => {
         <Oicon/>
       </div>
       <div className='card shadow-gray' >
-          
+          <h1 className='text-lg'>Pick player 1'st mark</h1>
+          <div className='start__player'>
+            <span className={activeUser === 'x' ? 'start__player--active' : ''} onClick={() => setActiveUser('x')}>
+              <Xicon color={activeUser === 'x' ? 'dark' : 'light'}/>
+            </span>
+            <span className={activeUser === 'o' ? 'start__player--active' : ''} onClick={() => setActiveUser('o')}>
+              <Xicon color={activeUser === 'o' ? 'dark' : 'light'}/>
+            </span>
+          </div>
+            <p className='text-light text-normal'>remember: x goes first</p>
       </div>
+        <div className='start__btns'>
+          <button className='btn btn-yellow' onClick={() => handleStart('cpu')}>
+            new game vs CPU
+          </button>
+          <button className='btn btn-blue' onClick={() => handleStart('user')}>
+            new game vs player
+          </button>
+        </div>
+
     </div>
   )
 }
